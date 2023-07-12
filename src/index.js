9a982e68fe655e96805a47dec0ac3d4bb0bdcbcf
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-07-12 18:20:23 +0200

    update

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@
 
 import axios from 'axios';
 
-console.log(axios.isCancel('something'));
+const select = document.querySelector('.breed-select');
+
+const apiKey =
+  'live_VmDtYrbpf1ukWdNjYyiTKw9CRmmlBLZ8W01P36GtuxdccK2dNzVBnrdDdch5MJgS';
