3af5a15b3cc50f1d33fb9ba88ceb2a2282479964
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-07-13 16:13:23 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,78 @@
 'use strict';
 
-import { fetchBreeds } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-const selectCats = document.querySelector('.breed-select');
+const select = document.querySelector('.breed-select');
+const catInfo = document.querySelector('.cat-info');
+const catImage = document.querySelector('#cat-image');
+const catName = document.querySelector('#cat-name');
+const catDescription = document.querySelector('#cat-description');
+const catTemperament = document.querySelector('#cat-temperament');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
-let catBreeds = [];
+function showLoader() {
+  loader.style.display = 'block';
+}
 
-fetchBreeds()
-  .then(cats => catBreeds.push(cats))
-  .catch(error => console.log(error));
+function hideLoader() {
+  loader.style.display = 'none';
+}
+
+function showError() {
+  error.style.display = 'block';
+}
+
+function hideError() {
+  error.style.display = 'none';
+}
+
+function createOption(breeds) {
+  select.innerHTML = breeds
+    .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
+    .join('');
+}
+
+function displayCatInfo(cat) {
+  catImage.src = cat.url;
+  catName.textContent = cat.breeds[0].name;
+  catDescription.textContent = cat.breeds[0].description;
+  catTemperament.textContent = `Temperament: ${cat.breeds[0].temperament}`;
+
+  catInfo.style.display = 'block';
+}
+
+function selectCat() {
+  const selectedBreedId = select.value;
+
+  if (selectedBreedId) {
+    showLoader();
+
+    fetchCatByBreed(selectedBreedId)
+      .then(cat => {
+        displayCatInfo(cat);
+        hideLoader();
+      })
+      .catch(error => {
+        showError();
+        hideLoader();
+      });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  showLoader();
+  hideError();
+
+  fetchBreeds()
+    .then(breeds => {
+      createOption(breeds);
+      hideLoader();
+    })
+    .catch(error => {
+      showError();
+      hideLoader();
+    });
+
+  select.addEventListener('change', selectCat);
+});
