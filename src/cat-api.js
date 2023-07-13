932451b7f8c836f9a07a9c7646396c6aa3253eaa
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-07-13 13:27:48 +0200

    update

---
+++ src/cat-api.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const userName = 'mirek';
+export { fetchBreeds };
+
+function fetchBreeds() {
+  fetch('https://api.thecatapi.com/v1/breeds')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      console.log(response);
+      return response.json();
+    })
+    .catch(error => {
+      return console.log(error);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 'use strict';
 
 import axios from 'axios';
+import { fetchBreeds } from './cat-api';
+
+let catBreeds = [];
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_VmDtYrbpf1ukWdNjYyiTKw9CRmmlBLZ8W01P36GtuxdccK2dNzVBnrdDdch5MJgS';
+
+fetchBreeds()
+  .then(cats => catBreeds.push(cats))
+  .catch(error => console.log(error));
